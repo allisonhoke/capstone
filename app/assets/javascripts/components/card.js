d5ca043fea121fc5e06658e9b6b87d89fffb942f
Allison Hoke <allisonhoke@gmail.com>
2017-01-02 12:37:54 -0800

    WIP setting up card component

---
+++ app/assets/javascripts/components/card.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+//
+// class Card extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       value: null
+//     };
+//   }
+//   render() {
+//     return (
+//       <button>
+//         {1234}
+//       </button>
+//     );
+//   }
+// }
