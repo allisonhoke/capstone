69078480ecdb8379305bd30d3480be1cb679c71e
Allison Hoke <allisonhoke@gmail.com>
2017-01-02 14:22:48 -0800

    wrote comments to myself to lay out what the controller actions should do

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,23 @@
 class GamesController < ApplicationController
+  def index
+    # find a random card set from the database
+    # make it an @variable that you will referece in the view
+    # the object will have properties that you reference by
+    # object["attribute(key in the DB)"]
+  end
+
+  def create
+    # @game = ModelObject.new(game_params)
+    # if @game.insert_document
+    # => render json: @game
+    # else
+    # => head 500 # OR OTHER ERROR CODE
+    # end
+  end
+
+  # private
+  #
+  # def game_params
+  #   params.require(:game).permit(:timestart, :timefinish, :level, :user)
+  # end
 end
