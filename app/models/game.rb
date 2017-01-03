5cec01b708225fcb869eca223f9e67bcfaf138d9
Allison Hoke <allisonhoke@gmail.com>
2017-01-02 18:22:07 -0800

    added comments with ideas for functionality

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,15 @@ class Game
   field :timefinish, type: Date
   field :level, type: Fixnum
   field :user, type: String
+
+  # def insert_document #passed in as hash
+  #   client = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'math-game')
+  #   collection = client[:gamesCollection]
+  #
+  #   if collection.insert_one(title: self.title, date: Time.now, amount: self.amount)
+  #     return true
+  #   else
+  #     return false
+  #   end
+  # end
 end
