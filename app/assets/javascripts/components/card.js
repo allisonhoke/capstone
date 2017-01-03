847d20f99ef885f86402998b8e68770628165966
Allison Hoke <allisonhoke@gmail.com>
2017-01-02 18:23:59 -0800

    WIP adjusting card because state does not need to change - only needs to display its value

--- app/assets/javascripts/components/card.js
+++ app/assets/javascripts/components/card.js
@@ -15,3 +15,23 @@
 //     );
 //   }
 // }
+
+// TODO: adjust the card so that it only displays its value - there is no need for anything to happen when it is clicked
+
+var Card = React.createClass({
+  getInitialState: function() {
+    return {value: 0};
+  },
+  // handleClick: function() {
+  //   this.setState({value: this.state.value + 1});
+  // },
+  render: function() {
+    return React.DOM.div(
+      null,
+      // {onClick: this.handleClick},
+      this.state.value
+    );
+  }
+});
+
+// React.renderComponent(Card(), mountNode);
