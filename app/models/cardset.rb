ae5b1c7149185328ac9a93ff60f97bccc320c026
Allison Hoke <allisonhoke@gmail.com>
2017-01-12 14:26:50 -0800

    wrote method for turning final board values into string of only the values from the number cards

--- app/models/cardset.rb
+++ app/models/cardset.rb
@@ -2,7 +2,26 @@ class Cardset
   include Mongoid::Document
   field :set, type: Array
 
+
+  def self.get_nums_only(final_board)
+    board = final_board  #array of hash {value: "n"}
+    values = []
+
+    #put the values in the array
+    board.each do |card| #each card is a object/hash {value: "value"}
+      if card[:value] =~ /\d/
+        values << card[:value]
+      end
+    end
+
+    return values #array of strings
+  end
+
+
   def already_in_collection
-    
+    client = Mongoid::Clients.default
+    collection = client[:cardsets]
+
+
   end
 end
--- spec/models/cardset_spec.rb
+++ spec/models/cardset_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Cardset, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  it "#already_in_collection should return true if the cardset is already in the collection" do
-    test_cardset = Game.new(cardset: [{value: "3"}, {value: "+"}, {value: "2"}, {value: "="}, {value: "5"}])
+  it "self.get_nums_only should return the correct array" do
+    test_board = [{value: "3"}, {value: "+"}, {value: "5"}, {value: "="}, {value: "8"}]
 
-    expect(test_cardset.already_in_collection).to eq(true)
+    expect(Cardset.get_nums_only(test_board)).to eq(["3", "5", "8"])
   end
+
+  # it "#already_in_collection should return true if the cardset is already in the collection" do
+  #   test_cardset = Cardset.new(set: [{value: "3"}, {value: "2"}, {value: "5"}, {value: "1"}, {value: "8"}])
+  #
+  #   expect(test_cardset.already_in_collection).to eq(true)
+  # end
 end
