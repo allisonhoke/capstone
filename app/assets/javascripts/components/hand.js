6d8060ac36fc8931009748a2081d29c3753b7fa5
Allison Hoke <allisonhoke@gmail.com>
2017-01-23 20:49:24 -0800

    can move cards out of hand WIP moving cards back into hand

--- app/assets/javascripts/components/hand.js
+++ app/assets/javascripts/components/hand.js
@@ -29,13 +29,39 @@ var Hand = React.createClass({
     e.dataTransfer.setData('card', JSON.stringify({value: e.currentTarget.innerHTML}));
     // console.log(e.dataTransfer.getData('card'));
   },
+  removeChild: function() {
+    if (this.state.current_card !== null) {
+      for (var i = 0; i < this.state.card_array.length; i++) {
+          if (this.state.card_array[i].value == this.state.current_card.value) {
+            // console.log("removing index: " + i + " with value: " + this.state.card_array[i].value);
+            this.state.card_array.splice(i, 1);
+          }
+        }
+        this.current_card = null;
+        //set the state, which re-renders the hand with the correct cards in it
+        this.setState(this.state);
+        // console.log("FINAL HAND: " + JSON.stringify(this.state.card_array));
+    }
+  },
+  onDragLeaveContainer: function(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    var top    = e.currentTarget.offsetTop;
+    var bottom = top + e.currentTarget.offsetHeight;
+    var left   = e.currentTarget.offsetLeft;
+    var right  = left + e.currentTarget.offsetWidth;
+    if (y <= top || y >= bottom || x <= left || x >= right) {
+      // console.log("dragleavecontainer");
+      this.removeChild();
+    }
+  },
   render: function() {
     // console.log(JSON.stringify(this.state));
     // if there is anything in the hand, render the cards
     if (this.state.card_array.length > 0) {
       return React.createElement(
         'ul',
-        {className: "hand row small-up-8", onDragOver: this.dragOver, onDragLeave: this.onDragLeaveContainer, onDrop: this.drop},
+        {className: "hand row small-up-8", onDragOver: this.dragOver, onDrop: this.drop, onDragLeave: this.onDragLeaveContainer},
         this.state.card_array.map(function(cardNumber) {
           // console.log("key is: " + key);
           return React.createElement(
